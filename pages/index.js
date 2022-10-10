f44f04ddaf35290872253a1163af50bcc6c1515d
Akankshagit22 <115221158+Akankshagit22@users.noreply.github.com>
2022-10-10 19:13:44 +0530

    Meta changes

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,13 @@ export default function Home() {
         <meta name="description" content="An artistic take on the organizational structure & activities of Akanksha Foundation through the eyes of Dhanashree Birajdar"/>
 
         <meta property="og:type" content="website"/>
-        <meta property="og:url" content="https://annualreport.akanksha.org/introduction"/>
+        <meta property="og:url" content="https://annualreport.akanksha.org/"/>
         <meta property="og:title" content="Akanksha's Annual Report 21-22"/>
         <meta property="og:description" content="An artistic take on the organizational structure & activities of Akanksha Foundation through the eyes of Dhanashree Birajdar"/>
         <meta property="og:image" content="https://annualreport.akanksha.org/meta-image.png"/>
 
         <meta property="twitter:card" content="summary_large_image"/>
-        <meta property="twitter:url" content="https://annualreport.akanksha.org/introduction"/>
+        <meta property="twitter:url" content="https://annualreport.akanksha.org/"/>
         <meta property="twitter:title" content="Akanksha's Annual Report 21-22"/>
         <meta property="twitter:description" content="An artistic take on the organizational structure & activities of Akanksha Foundation through the eyes of Dhanashree Birajdar"/>
         <meta property="twitter:image" content="https://annualreport.akanksha.org/meta-image.png"/>
