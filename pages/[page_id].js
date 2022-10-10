67901cdee729c644ddafa16baea26096634198ec
Akankshagit22 <115221158+Akankshagit22@users.noreply.github.com>
2022-10-10 20:03:55 +0530

    Meta Updates v2

--- pages/[page_id].js
+++ pages/[page_id].js
@@ -121,19 +121,19 @@ function FullPage() {
     
       <div className={styles.container} id="main_content">
         <Head>
-        <title>Akanksha&apos;s Annual Report 21-22</title>
+        <title>Akanksha&apos;s Annual Report 21-22 | Akanksha Foundation</title>
         <meta name="title" content="Akanksha's Annual Report 21-22"/>
         <meta name="description" content="An artistic take on the organizational structure & activities of Akanksha Foundation through the eyes of Dhanashree Birajdar"/>
 
         <meta property="og:type" content="website"/>
-        <meta property="og:url" content="https://annualreport.akanksha.org/introduction"/>
-        <meta property="og:title" content="Akanksha's Annual Report 21-22"/>
+        <meta property="og:url" content="https://annualreport.akanksha.org/"/>
+        <meta property="og:title" content="Akanksha&apos;s Annual Report 21-22 | Akanksha Foundation"/>
         <meta property="og:description" content="An artistic take on the organizational structure & activities of Akanksha Foundation through the eyes of Dhanashree Birajdar"/>
         <meta property="og:image" content="https://annualreport.akanksha.org/meta-image.png"/>
 
         <meta property="twitter:card" content="summary_large_image"/>
-        <meta property="twitter:url" content="https://annualreport.akanksha.org/introduction"/>
-        <meta property="twitter:title" content="Akanksha's Annual Report 21-22"/>
+        <meta property="twitter:url" content="https://annualreport.akanksha.org/"/>
+        <meta property="twitter:title" content="Akanksha&apos;s Annual Report 21-22 | Akanksha Foundation"/>
         <meta property="twitter:description" content="An artistic take on the organizational structure & activities of Akanksha Foundation through the eyes of Dhanashree Birajdar"/>
         <meta property="twitter:image" content="https://annualreport.akanksha.org/meta-image.png"/>
           <link rel="icon" href="/favicon.ico" />
